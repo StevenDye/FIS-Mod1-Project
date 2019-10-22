205b46c836b53519b4e646aacdee42eeb2aaca55
Allison Lee <allison.alee@gmail.com>
2019-10-22 15:36:20 -0400

    Added another function

--- project/functions_for_cleaning_data.py
+++ project/functions_for_cleaning_data.py
@@ -1,9 +1,13 @@
 """ This file contains functions used to clean datasets in FinalMod1Proj."""
 
 def clean(df, col):
-"""Cleans commas and dollar signs, converts to integer, and returns rounded numbers in millions of dollars"""
+    """Cleans commas and dollar signs and converts to integer"""
+    import numpy as np
     df[col] = df[col].str.replace(',','')
     df[col] = df[col].str.replace('$','')
     df[col] = df[col].astype('int64')
-    df[col] = np.round(df[col].map(lambda x: x/1_000_000),2)
     return df.head()
+
+def to_millions_usd(df, col):
+    import numpy as np
+    return df[col] = np.round(df[col].map(lambda x: x/1_000_000),2)
